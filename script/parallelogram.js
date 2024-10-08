9d6b789669261a4db5879b2a5517ae8cc63f222c
Minarajahan <meherinsultanaminara@gmail.com>
2024-10-08 21:00:01 +0800

    updated

--- script/parallelogram.js
+++ script/parallelogram.js
@@ -1,3 +1,26 @@
-function parallelogram(){
-    console.log('conated');
+function parallelogram() {
+    // get area input
+    const areaParalelogramInput = document.getElementById('area');
+    const takeValue = areaParalelogramInput.value;
+    const mainValue = parseFloat(takeValue);
+    console.log(mainValue);
+
+    // get area height
+    const heightParalelogramInput = document.getElementById('height');
+    const heightValue = heightParalelogramInput.value;
+    const exactHeightValue = parseFloat(heightValue);
+    console.log(exactHeightValue);
+
+    // calculation
+    const calculateValue=mainValue*heightValue;
+    console.log(calculateValue);
+
+    // Display Value
+
+    const displayValue = document.getElementById('inerValueDisplay');
+    displayValue.innerText=calculateValue;
+    console.log(showInnerValue);
+
+
+
 }
